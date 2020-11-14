6a85a001960e2ec62aefb46f01e5eedd701ae891
Lucas <lucas.maia@mydoctorapp.com.br>
2020-11-14 20:02:21 -0300

    feat:import express-async-errors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,10 +4,12 @@ import express, {NextFunction, Response, Request} from 'express'
 import cors from 'cors'
 import AppError from "@shared/errors/AppError";
 import {createConnection} from "typeorm";
+import 'express-async-errors';
 
 import routes from "@shared/infra/http/routes";
 import '@shared/infra/typeorm';
 import "@shared/container"
+import {errors} from "celebrate";
 
 createConnection()
 
@@ -17,6 +19,7 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
+app.use(errors())
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     if (err instanceof AppError) {
         return response.status(err.statusCode).json({
